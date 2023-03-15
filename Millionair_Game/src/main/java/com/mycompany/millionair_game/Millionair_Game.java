de6253c3f9edb1e1f29eb99c277215be070fb7ba
FabienneFly <fehrfab2@students.zhaw.ch>
2023-03-16 12:39:19 +1300

    First Classes

---
+++ Millionair_Game/src/main/java/com/mycompany/millionair_game/Millionair_Game.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.millionair_game;
+
+/**
+ *
+ * @author FabiF
+ */
+public class Millionair_Game {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
